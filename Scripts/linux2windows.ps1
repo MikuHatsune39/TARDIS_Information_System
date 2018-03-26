b363508fd249d27c433726193b1b797186e0f1b7
MikuHatsune39 <32179075+MikuHatsune39@users.noreply.github.com>
2018-03-25 21:51:22 -0700

    Add files via upload

--- Scripts/linux2windows.ps1
+++ Scripts/linux2windows.ps1
@@ -1,1 +1,3 @@
 ﻿(Get-Content "main.c") | Set-Content "main.c"
+(Get-Content "main.c") | ForEach-Object { $_ -replace "clear", "cls" } | Set-Content "main.c"
+(Get-Content "main.c") | ForEach-Object { $_ -replace "./db/", ".\\db\\" } | Set-Content "main.c"
